995d79287869264bf391a33dced7b4048a29819a
draffault <draffault@gmail.com>
2018-03-06 00:01:39 +0100

    teleinfo.py

--- python/teleinfo.py
+++ python/teleinfo.py
@@ -2,14 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import os
-import serial #pip install pyserial
+import serial # pip install pyserial
 import subprocess
+import mysql.connector # pip install mysql-connector-python --allow-external mysql-connector-python
 
 #PORT_ID = "/dev/ttyUSB0"
 PORT_ID = "COM4"
 DB_NAME = "teleinfo"
 
 def saveTrame(trameMap):
+  conn = mysql.connector.connect(host="192.168.0.36", user="teleinfo", password="teleinfo", database="teleinfo")
+  cursor = conn.cursor()
+ 
+  # user = ("olivier", "34")
+  # cursor.execute("""INSERT INTO users (name, age) VALUES(%s, %s)""", user)
+
+  # user = {"name": "olivier", "age" : "34"}
+  # cursor.execute("""INSERT INTO users (name, age) VALUES(%(name)s, %(age)s)""", user)  
+  
+  conn.close()
   return true
 
 def analyseTrame(trame):
@@ -33,6 +44,7 @@ def analyseTrame(trame):
 def test():
   trame = "ADCO 030222707626 <\r\n\r\nOPTARIF BASE 0\r\n\r\nISOUSC 30 9\r\n\r\nBASE 094304690 .\r\n\r\nPTEC TH.. $\r\n\r\nIINST 003 Z\r\n\r\nIMAX 032 D\r\n\r\nPAPP 00740 ,\r\n\r\nMOTDETAT 000000 B\r\n\r\n"
   trameMap = analyseTrame(trame)
+  saveTrame(trameMap)
 
 def readSerial(portId):
   try:
